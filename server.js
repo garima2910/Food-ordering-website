2abe591a25dba18e74f98292e34d8c2a980b7a59
garima2910 <garimdhall15@gmail.com>
2020-09-03 20:23:04 +0530

    cart page

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const expressLayout=require('express-ejs-layouts');
 const app=express();
 const port =process.env.port||3000
 app.use(express.static('public'));
-app.get('/',function(req,res){
-    res.render('home');
-})
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs');
+app.get('/',function(req,res){
+    res.render('home');
+})
+app.get('/cart',function(req,res){
+    res.render('customers/cart');
+})
+
 app.listen(port,()=>{
     console.log("listening on port "+port)
 })
