c860ece6652d66d58088662b39e3415e20d5b525
garima2910 <garimdhall15@gmail.com>
2020-09-03 14:36:02 +0530

    updated app.js

--- public/js/app.js
+++ public/js/app.js
@@ -115,8 +115,8 @@ console.log("hello from app js");
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\dhall\Desktop\dev\Food ordering website\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\dhall\Desktop\dev\Food ordering website\resources\scss\app.scss */"./resources/scss/app.scss");
+__webpack_require__(/*! C:\Users\dhall\Documents\GitHub\Food-ordering-website\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\dhall\Documents\GitHub\Food-ordering-website\resources\scss\app.scss */"./resources/scss/app.scss");
 
 
 /***/ })
